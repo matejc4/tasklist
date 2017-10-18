ace05e5f09666b3a19bfeb01ed45a28d1edf9ea4
Matej Čičak <matej.cicak44@gmail.com>
2017-10-18 21:06:38 +0200

    napravljen master blade view app

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="hr">
+	<head>
+		<title>TASK app - Laravel vježba</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<!-- -->
+			</nav>
+		</div>
+		@yield('content')
+	</body>
+</html>
